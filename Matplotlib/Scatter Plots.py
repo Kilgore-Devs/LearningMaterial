db84a563e76efe822d6cc6c3dc8e96fed4058789
Kilgore_Devs <Dr.Dev.Kilgore@gmail.com>
2023-08-11 15:05:26 -0500

    Added how to change colors of each dot.

--- Matplotlib/Scatter Plots.py
+++ Matplotlib/Scatter Plots.py
@@ -4,9 +4,16 @@ import numpy as np
 x = np.array([1, 2, 3, 4, 5, 8, 10, 8, 5, 4, 3, 2, 1])
 y = np.array([1, 2, 3, 4, 5, 20, 50, 80, 95, 96, 97, 98, 99])
 plt.scatter(x, y, color='black')  # can change color of plots with name or hex value
+
+""" can set each specific color for each dot by...
+np.array(["red","green","blue","yellow"...])
+plt.scatter(x, y, c=colors)
+"""
+
 # can add a second set to compare
 x = np.array([2, 4, 6, 8, 10, 12, 14, 12, 10, 8, 6, 4, 2])
 y = np.array([1, 2, 3, 4, 5, 20, 50, 80, 95, 96, 97, 98, 99])
+
 plt.scatter(x, y, color='red')  # can change color of plots with name or hex value
 
 plt.show()
