85c1dfa1d7bdb06f1233e21f191773b148ebf2d3
Kilgore_Devs <Dr.Dev.Kilgore@gmail.com>
2023-08-11 16:49:03 -0500

    Using pie charts.

---
+++ Matplotlib/Pie Chart.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+y = np.array([30, 10, 45, 15])  # starts at first wedge on right going counterclockwise. Default angle is 0.
+thelabels = ["Carbs", "Fats", "Protein", "Fiber"]
+pullawayfrompie = [.1, .1, .5, .1]  # explode is used in plt.pit to pull pieces out.
+thecolors = ["Bisque", "Yellow", "Brown", "Green"]
+
+plt.pie(y, labels=thelabels, startangle=90, explode=pullawayfrompie, shadow=True, colors=thecolors)
+plt.legend(title = "Daily Consumption")  # adds a legend, and title inside()
+plt.show()
+
+
+
+
+
