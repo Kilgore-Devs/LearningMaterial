28a86eb8609d0bd8ca74912d3414d7dc5cf63c4a
Kilgore_Devs <Dr.Dev.Kilgore@gmail.com>
2023-08-16 10:15:53 -0500

    How to use lists.

---
+++ basics/list exercise.py
@@ -0,0 +1,4 @@
+mylist = [1, 2.2, False, [10, 9, 8]]  # create var and assign list of diff types
+called_list = list(mylist)  # create another vat and assign it a call of list fx with a str as arg
+print("e" in called_list)  # checks if e is in the list of 2nd var
+print("a" not in called_list)  # checks if a is not in var2
